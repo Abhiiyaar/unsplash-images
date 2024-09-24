098310062cca92681f77256d1f6d37093d51e2e5
Abhiiyaar <itsabhigandhi@gmail.com>
2024-09-24 21:58:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Gallery from "./Gallery";
+import SearchForm from "./Searchform";
+import ThemeToggle from "./ThemeToggle";
+
+const App = () => {
+  return (
+    <main>
+      <ThemeToggle />
+      <SearchForm />
+      <Gallery />
+    </main>
+  );
+};
+export default App;
